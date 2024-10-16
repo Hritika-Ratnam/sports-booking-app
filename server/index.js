2412078d4f13cea8b8b2b3895243c03387c62fb2
Hritika Ratnam <najukreso@gmail.com>
2024-10-17 03:17:10 +0530

    add cors support

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const corsOptions = {
     methods: 'GET,POST,PUT,DELETE',  // Specify the allowed HTTP methods
     optionsSuccessStatus: 200  // For legacy browser support
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json()); // Middleware to parse incoming requests as JSON
 
 // API endpoint to view bookings by center, sport, and date
